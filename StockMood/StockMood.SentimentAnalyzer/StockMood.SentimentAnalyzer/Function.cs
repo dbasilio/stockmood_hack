1a6a8c665850bc8aa26ff42bf365dc89d1d002e7
Maxim Volembovschii <maximv@q4inc.com>
2017-02-24 11:27:28 -0600

    empty lambda

---
+++ StockMood/StockMood.SentimentAnalyzer/StockMood.SentimentAnalyzer/Function.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace StockMood.SentimentAnalyzer
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public void FunctionHandler(ILambdaContext context)
+        {
+            context.Logger.LogLine("Sentiment analyzis success!");
+        }
+    }
+}
