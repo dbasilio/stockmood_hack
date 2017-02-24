e36e324d6de4c383e830d5db208c56869fa6b54b
Daniel Basilio <daniel.basilio90@gmail.com>
2017-02-24 11:08:53 -0600

    pulling tweets and stuff

--- StockMood/StockMood.TwitterGrabber/Function.cs
+++ StockMood/StockMood.TwitterGrabber/Function.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -21,7 +23,20 @@ namespace StockMood.TwitterGrabber
         /// <returns></returns>
         public void FunctionHandler(ILambdaContext context)
         {
-            context.Logger.LogLine("twittergrabber checking in.");
+            ITwitterCredentials creds = new TwitterCredentials("LeWMQwaGHAvkiGDzOaCwWnqDg",
+                "O9huoRD17SbQlkHDaTtvOsJ2g3qoMXr9n9qaLKNKGesVp2FnOZ",
+                "835155580037775360-k78JNWd4QiH3JVAqgpzzEBFgNYsYfp8", "PMRySX5hG2eTzilbK9ZIrVWQjs8fVZzfqvkD8A1rc7emP");
+            Auth.SetCredentials(creds);
+
+            Tweet.PublishTweet(DateTime.Now.ToString());
+
+            var tweets = Search.SearchTweets("MSFT").Take(10);
+
+            foreach (var tweet in tweets)
+            {
+                context.Logger.LogLine(tweet.Text);
+            }
+            context.Logger.LogLine("finished running");
         }
     }
 }
