46df08fb544fba0203f458daed84712289064816
Maxim Volembovschii <maximv@q4inc.com>
2017-02-24 12:15:52 -0600

    bla

--- StockMood/StockMood.SentimentAnalyzer/StockMood.SentimentAnalyzer/Function.cs
+++ StockMood/StockMood.SentimentAnalyzer/StockMood.SentimentAnalyzer/Function.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization;
+using Newtonsoft.Json;
+using StockMood.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -22,6 +25,9 @@ namespace StockMood.SentimentAnalyzer
         /// <returns></returns>
         public void FunctionHandler(ILambdaContext context)
         {
+            var tweets = new List<TweetDto>();
+            var tweetsCoolection = JsonConvert.DeserializeObject<List<TweetDto>>(File.ReadAllText("tweets.json"));
+
             context.Logger.LogLine("Sentiment analyzis success!");
         }
     }
