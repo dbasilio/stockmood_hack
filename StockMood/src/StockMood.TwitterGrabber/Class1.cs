e213b6bb917b1c36d991efa89acbaffc01fa6854
Daniel Basilio <daniel.basilio90@gmail.com>
2017-02-24 10:15:13 -0600

    replacing project with lambda one

--- StockMood/src/StockMood.TwitterGrabber/Class1.cs
+++ StockMood/src/StockMood.TwitterGrabber/Class1.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace StockMood.TwitterGrabber
+﻿namespace StockMood.TwitterGrabber
 {
     public class Class1
     {
