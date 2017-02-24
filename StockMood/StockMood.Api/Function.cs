251b2c49ce36eec0053611a6aa12fc135185d5a7
Daniel Basilio <daniel.basilio90@gmail.com>
2017-02-24 14:42:49 -0600

    start of the api

---
+++ StockMood/StockMood.Api/Function.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Amazon.Runtime;
+using StockMood.Models;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace StockMood.Api
+{
+    public class Function
+    {
+        public IEnumerable<TweetDto> GetTweets(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            var dynamoDbClient =
+                new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAITP2P5WZV5HT4UYA",
+                    "xa3m+F2i9QsHckUkDz+o60RpFO71PAtRWvH+8+eK"));
+            var dbContext = new DynamoDBContext(dynamoDbClient);
+            return null;
+        }
+
+        public int GetPublicSentiment()
+        {
+            var dynamoDbClient =
+                new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAITP2P5WZV5HT4UYA",
+                    "xa3m+F2i9QsHckUkDz+o60RpFO71PAtRWvH+8+eK"));
+            var dbContext = new DynamoDBContext(dynamoDbClient);
+
+            var tweets = dbContext.ScanAsync<TweetDto>(new ScanCondition[] {}).GetRemainingAsync().Result;
+            var count = tweets.Count;
+
+            //var positive = tweets.Where(x => x.)
+            
+            return 0;
+        }
+
+        public TweetDto GetMostTrending()
+        {
+            var dynamoDbClient =
+                new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAITP2P5WZV5HT4UYA",
+                    "xa3m+F2i9QsHckUkDz+o60RpFO71PAtRWvH+8+eK"));
+            var dbContext = new DynamoDBContext(dynamoDbClient);
+
+
+            return null;
+        }
+    }
+}
